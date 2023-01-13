dd7c4fde6d9b96a34a6cca94337929d0b9b79929
fosterm-mw <michael.foster@mavenwave.com>
2023-01-13 14:53:43 -0600

    fixed packages, new k8s plugin soon

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -55,7 +55,7 @@ import {
 
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
-import { EntityArgocdAutopilotContent } from '@internal/plugin-argocd-autopilot';
+// import { EntityArgocdAutopilotContent } from '@internal/plugin-argocd-autopilot';
 import {
     EntityArgoCDOverviewCard,
     isArgocdAvailable
