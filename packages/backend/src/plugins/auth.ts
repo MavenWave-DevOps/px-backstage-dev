a9a36cd0db996952fce8e85edc26d8c43b96c368
fosterm-mw <michael.foster@mavenwave.com>
2023-04-07 10:23:42 -0500

    azure accounts access to backstage

--- packages/backend/src/plugins/auth.ts
+++ packages/backend/src/plugins/auth.ts
@@ -19,6 +19,31 @@ export default async function createPlugin(
     providerFactories: {
       ...defaultAuthProviderFactories,
 
+      microsoft: providers.microsoft.create({
+        signIn: {
+          resolver: async ({ profile }, ctx) => {
+            if (!profile.email) {
+              throw new Error(
+                  'Login failed, user profile does not contain an email',
+              );
+            }
+            const [localPart] = profile.email.split('@')
+            const userEntityRef = stringifyEntityRef({
+              kind: 'User',
+              name: localPart,
+              namespace: DEFAULT_NAMESPACE,
+            });
+
+            return ctx.issueToken({
+              claims: {
+                sub: userEntityRef,
+                ent: [userEntityRef],
+              },
+            });
+          },
+        },
+      }),
+
       google: providers.google.create({
         signIn: {
           resolver: async ({ profile }, ctx) => {
