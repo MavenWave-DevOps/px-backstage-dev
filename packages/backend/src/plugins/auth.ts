e406bba41349f741845cfb612da0f491233d2212
sankd <santosh.dubey@mavenwave.com>
2023-05-02 20:55:37 +0530

    template path update

--- packages/backend/src/plugins/auth.ts
+++ packages/backend/src/plugins/auth.ts
@@ -19,30 +19,37 @@ export default async function createPlugin(
     providerFactories: {
       ...defaultAuthProviderFactories,
 
-      microsoft: providers.microsoft.create({
+       microsoft: providers.microsoft.create({
         signIn: {
-          resolver: async ({ profile }, ctx) => {
-            if (!profile.email) {
-              throw new Error(
-                  'Login failed, user profile does not contain an email',
-              );
-            }
-            const [localPart] = profile.email.split('@')
-            const userEntityRef = stringifyEntityRef({
-              kind: 'User',
-              name: localPart,
-              namespace: DEFAULT_NAMESPACE,
-            });
-
-            return ctx.issueToken({
-              claims: {
-                sub: userEntityRef,
-                ent: [userEntityRef],
-              },
-            });
-          },
+          resolver:
+            providers.microsoft.resolvers.emailMatchingUserEntityAnnotation(),
         },
       }),
+
+      // microsoft: providers.microsoft.create({
+      //   signIn: {
+      //     resolver: async ({ profile }, ctx) => {
+      //       if (!profile.email) {
+      //         throw new Error(
+      //             'Login failed, user profile does not contain an email',
+      //         );
+      //       }
+      //       const [localPart] = profile.email.split('@')
+      //       const userEntityRef = stringifyEntityRef({
+      //         kind: 'User',
+      //         name: localPart,
+      //         namespace: DEFAULT_NAMESPACE,
+      //       });
+
+      //       return ctx.issueToken({
+      //         claims: {
+      //           sub: userEntityRef,
+      //           ent: [userEntityRef],
+      //         },
+      //       });
+      //     },
+      //   },
+      // }),
 
       google: providers.google.create({
         signIn: {
