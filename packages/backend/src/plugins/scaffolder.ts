8ade6fdce0b8748591f602b00741cc84564f62ee
fosterm-mw <michael.foster@mavenwave.com>
2023-05-09 21:20:47 -0500

    start testing ado pr action

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,7 +1,13 @@
 import { CatalogClient } from '@backstage/catalog-client';
 import { createRouter } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
+import { 
+  cloneAzureRepoAction,
+  pushAzureRepoAction
+} from "@parfuemerie-douglas/scaffolder-backend-module-azure-repositories";
+// import { createAzurePullRequest } from '../../../../../../custom/backstage-plugin-scaffolder-backend-module-azure-devops-repositories/src/actions/run/createAzurePullRequest';
 import type { PluginEnvironment } from '../types';
+import { ScmIntegrations } from '@backstage/integration';
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -9,6 +15,20 @@ export default async function createPlugin(
   const catalogClient = new CatalogClient({
     discoveryApi: env.discovery,
   });
+
+  const integrations = ScmIntegrations.fromConfig(env.config)
+
+  const actions = [
+    cloneAzureRepoAction({ integrations }),
+    pushAzureRepoAction({ integrations, config: env.config }),
+    // ...createBuiltInActions({
+    //   containerRunner,
+    //   catalogClient,
+    //   integrations,
+    //   config: env.config,
+    //   reader: env.reader,
+    // }),
+  ]
 
   return await createRouter({
     logger: env.logger,
