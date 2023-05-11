c9f6802fef76aecbe337b352521ff868970cb34c
fosterm-mw <michael.foster@mavenwave.com>
2023-05-11 13:50:52 -0500

    use mc-203 for scaffolder

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,11 +1,13 @@
 import { CatalogClient } from '@backstage/catalog-client';
-import { createBuiltinActions, createRouter } from '@backstage/plugin-scaffolder-backend';
+import { createRouter, createBuiltinActions } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
-import { ScmIntegrations } from "@backstage/integration";
-import { cloneAdRepoAction, pushAdRepoAction } from './scaffolder/actions';
-import { createHttpBackstageAction } from './scaffolder/actions/execute/createPullRequest';
-// import { commitAndPushADOBranch } from './scaffolder/actions/execute/createPullRequest';
+import { ScmIntegrations } from '@backstage/integration';
+import {
+    cloneAzureRepoAction,
+    pushAzureRepoAction,
+    pullRequestAzureRepoAction,
+} from "@fostemi/scaffolder-backend-module-azure-repositories";
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -13,33 +15,29 @@ export default async function createPlugin(
   const catalogClient = new CatalogClient({
     discoveryApi: env.discovery,
   });
+  const integrations = ScmIntegrations.fromConfig(env.config)
 
-  const integrations = ScmIntegrations.fromConfig(env.config);
+  const builtInActions = createBuiltinActions({
+    integrations,
+    catalogClient,
+    config: env.config,
+    reader: env.reader,
+  });
 
   const actions = [
-    cloneAdRepoAction({ integrations }),
-    pushAdRepoAction({ integrations, config: env.config }),
-    createHttpBackstageAction({discovery: env.discovery}),
-    // commitAndPushADOBranch({ integrations, config: env.config }),
-    ...createBuiltinActions({
-      catalogClient,
-      config: env.config,
-      reader: env.reader,
-      integrations,
-
-    }),
-  ];
+    cloneAzureRepoAction({ integrations }),
+    pullRequestAzureRepoAction({ integrations }),
+    pushAzureRepoAction({ integrations, config: env.config }),
+    ...builtInActions
+  ]
 
   return await createRouter({
+    actions,
     logger: env.logger,
     config: env.config,
     database: env.database,
     reader: env.reader,
-    identity: env.identity,
     catalogClient,
-    actions,
+    identity: env.identity,
   });
-
-
-
 }
